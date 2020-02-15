de64815dafe16c88d7567f1fb362f4e8044e0ada
Najwab <inajjwa2@gmail.com>
2020-02-15 03:39:32 +0300

    completed

--- code/main.js
+++ code/main.js
@@ -1,2 +1,60 @@
 // If you get stuck go to axiox documentations
 // https://github.com/axios/axios
+
+//function for get the data and put them in new li 
+
+const urlb=`https://sei-relativity-ruh.herokuapp.com/developers`;
+
+function printData() {
+axios({
+    method:'get',
+    url:urlb
+
+})
+.then(function(res){
+    let arraList=res.data
+    for(let i=0;i<arraList.length;i++){
+    console.log(res.data[i].name);
+    let names=res.data[i].name;
+    let place =document.querySelector('#main');
+    var x = document.createElement("LI");
+    var t = document.createTextNode(names);
+     x.appendChild(t);
+     place.appendChild(x);
+    }
+})
+.catch(function(err){
+    console.log(err);
+})
+}
+//this function to add new one 
+
+function add(){
+    let inputValue=document.querySelector('#name').value;
+axios({
+    method:'post',
+    url:urlb,
+    data:{
+        name:inputValue
+    }
+
+})
+.then(function(res){
+    console.log(res.data);
+    
+    // let arraList=res.data
+    // for(let i=0;i<arraList.length;i++){
+    // console.log(res.data[i].name);
+    // let names=res.data[i].name;
+    // let place =document.querySelector('#main');
+    // var x = document.createElement("LI");
+    // var t = document.createTextNode(names);
+    //  x.appendChild(t);
+    //  place.appendChild(x);
+    // }
+})
+.catch(function(err){
+    console.log(err);
+})
+
+}
